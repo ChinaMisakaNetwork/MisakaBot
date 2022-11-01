d78cfe8a15930879cb0c8db1088cbc772135eaf8
御坂20002号 <91882141+misaka-20002@users.noreply.github.com>
2022-11-01 16:39:04 +0800

    Update install_depend.sh

--- install_depend.sh
+++ install_depend.sh
@@ -6,14 +6,14 @@ sudo apt install libmysql++-dev -y --force-yes -qq;
 sudo apt install cmake -y --force-yes -qq;
 cd /tmp;
 wget https://dev.mysql.com/get/Downloads/Connector-C++/libmysqlcppconn9_8.0.31-1ubuntu22.10_amd64.deb;
-dpkg -i libmysqlcppconn9_8.0.31-1ubuntu22.10_amd64.deb;
+sudo dpkg -i libmysqlcppconn9_8.0.31-1ubuntu22.10_amd64.deb;
 cd /usr/lib/x86_64-linux-gnu/
-ln -s libmysqlclient.so libmysqlclient_r.so
+sudo ln -s libmysqlclient.so libmysqlclient_r.so
 cd /tmp
 wget https://tangentsoft.com/mysqlpp/releases/mysql++-3.3.0.tar.gz;
 tar -zxvf mysql++-3.3.0.tar.gz;
 ln -s /usr/local/mysql/lib/mysql/libmysqlclient.so.15.0.0 /usr/local/mysql/lib/mysql/libmysqlclient_r.so
 cd mysql++-3.3.0
 sudo ./configure --prefix=/usr --prefix=/usr/local --enable-thread-check LDFLAGS='-pthread'
-mv Makefile.simple Makefile;
+sudo mv Makefile.simple Makefile;
 sudo make install
