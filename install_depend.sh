6570477de3a0b342bdd782066c169e0988a23f1b
御坂20002号 <91882141+misaka-20002@users.noreply.github.com>
2022-11-01 16:24:19 +0800

    Update install_depend.sh

--- install_depend.sh
+++ install_depend.sh
@@ -2,12 +2,14 @@ sudo apt update;
 sudo apt upgrade -y --force-yes -qq;
 sudo apt install g++ gcc -y --force-yes -qq;
 sudo apt install mysql-server -y --force-yes -qq;
-sudo apt install libmysqlclient-dev -y --force-yes -qq;
 sudo apt install libmysql++-dev -y --force-yes -qq;
+sudo apt install cmake -y --force-yes -qq;
 cd /tmp;
-wget https://dev.mysql.com/get/Downloads/Connector-C++/mysql-connector-c++-8.0.31-linux-glibc2.27-x86-64bit-debug.tar.gz
-tar -zxvf mysql-connector-c++-8.0.31-linux-glibc2.27-x86-64bit-debug.tar.gz
-cp mysql-connector-c++-8.0.31-linux-glibc2.27-x86-64bit-debug/lib64/* /usr/lib
+wget https://dev.mysql.com/get/Downloads/Connector-C++/libmysqlcppconn9_8.0.31-1ubuntu22.10_amd64.deb;
+dpkg -i libmysqlcppconn9_8.0.31-1ubuntu22.10_amd64.deb;
+cd /usr/lib/x86_64-linux-gnu/
+ln -s libmysqlclient.so libmysqlclient_r.so
+cd /tmp
 wget https://tangentsoft.com/mysqlpp/releases/mysql++-3.3.0.tar.gz;
 tar -zxvf mysql++-3.3.0.tar.gz;
 ln -s /usr/local/mysql/lib/mysql/libmysqlclient.so.15.0.0 /usr/local/mysql/lib/mysql/libmysqlclient_r.so
