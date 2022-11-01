4b3b7cc1f55a68c731fc0c7e876eafb01eec2933
御坂20002号 <91882141+misaka-20002@users.noreply.github.com>
2022-11-01 15:52:39 +0800

    Update install_depend.sh

--- install_depend.sh
+++ install_depend.sh
@@ -1,12 +1,16 @@
 sudo apt update;
 sudo apt upgrade -y --force-yes -qq;
-sudo apt install g++ gcc libgcc libg++ 
-sudo apt install mysql-server;
-sudo apt install libmysqlclient-dev;
-sudo apt install libmysql++-dev;
+sudo apt install g++ gcc -y --force-yes -qq;
+sudo apt install mysql-server -y --force-yes -qq;
+sudo apt install libmysqlclient-dev -y --force-yes -qq;
+sudo apt install libmysql++-dev -y --force-yes -qq;
 cd /tmp;
+wget https://dev.mysql.com/get/Downloads/Connector-C++/mysql-connector-c++-8.0.31-linux-glibc2.27-x86-64bit-debug.tar.gz
+tar -zxvf mysql-connector-c++-8.0.31-linux-glibc2.27-x86-64bit-debug.tar.gz
+cp mysql-connector-c++-8.0.31-linux-glibc2.27-x86-64bit-debug/lib64/* /usr/lib
 wget https://tangentsoft.com/mysqlpp/releases/mysql++-3.3.0.tar.gz;
 tar -zxvf mysql++-3.3.0.tar.gz;
+ln -s /usr/local/mysql/lib/mysql/libmysqlclient.so.15.0.0 /usr/local/mysql/lib/mysql/libmysqlclient_r.so
 cd mysql++-3.3.0
 sudo ./configure --prefix=/usr --prefix=/usr/local --enable-thread-check LDFLAGS='-pthread'
 mv Makefile.simple Makefile;
