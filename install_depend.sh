38bd66755156a24dc5d12340499c1bb7908ad9f4
御坂20002号 <91882141+misaka-20002@users.noreply.github.com>
2022-11-01 16:56:23 +0800

    Update install_depend.sh

--- install_depend.sh
+++ install_depend.sh
@@ -1,6 +1,7 @@
 sudo apt update;
 sudo apt upgrade -y --force-yes -qq;
-sudo apt install g++ gcc -y --force-yes -qq;
+sudo apt install g++ -y --force-yes -qq;
+sudo apt install gcc -y --force-yes -qq;
 sudo apt install mysql-server -y --force-yes -qq;
 sudo apt install libmysql++-dev -y --force-yes -qq;
 sudo apt install cmake systemtap-sdt-dev -y --force-yes -qq;
@@ -9,6 +10,7 @@ wget https://dev.mysql.com/get/Downloads/Connector-C++/libmysqlcppconn9_8.0.31-1
 sudo dpkg -i libmysqlcppconn9_8.0.31-1ubuntu22.10_amd64.deb;
 cd /usr/lib/x86_64-linux-gnu/
 sudo ln -s libmysqlclient.so libmysqlclient_r.so
+sudo ln -s /usr/include/x86_64-linux-gnu/sys/ /usr/include/
 cd /tmp
 wget https://tangentsoft.com/mysqlpp/releases/mysql++-3.3.0.tar.gz;
 tar -zxvf mysql++-3.3.0.tar.gz;
