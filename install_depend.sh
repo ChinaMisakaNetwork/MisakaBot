7f77e035d6456e4786be83a181c258cb1a53f920
御坂20002号 <91882141+misaka-20002@users.noreply.github.com>
2022-11-01 16:43:03 +0800

    Update install_depend.sh

--- install_depend.sh
+++ install_depend.sh
@@ -12,7 +12,6 @@ sudo ln -s libmysqlclient.so libmysqlclient_r.so
 cd /tmp
 wget https://tangentsoft.com/mysqlpp/releases/mysql++-3.3.0.tar.gz;
 tar -zxvf mysql++-3.3.0.tar.gz;
-ln -s /usr/local/mysql/lib/mysql/libmysqlclient.so.15.0.0 /usr/local/mysql/lib/mysql/libmysqlclient_r.so
 cd mysql++-3.3.0
 sudo ./configure --prefix=/usr --prefix=/usr/local --enable-thread-check LDFLAGS='-pthread'
 sudo mv Makefile.simple Makefile;
